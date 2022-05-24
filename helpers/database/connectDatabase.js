5c1438d0943778633e886a08237120731474ecde
İsmail Keyvan <58604123+Keyvan14162@users.noreply.github.com>
2022-05-24 13:59:41 +0300

    Update connectDatabase.js

--- helpers/database/connectDatabase.js
+++ helpers/database/connectDatabase.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 
 const connectDatabase = ()=> {
-    mongoose.connect("mongodb+srv://ismail_keyvan:Kamilkoc14162@cluster0.cah1y.mongodb.net/answerSporComment?retryWrites=true&w=majority")
-    .then(() => {
+    mongoose.connect("mongodb+srv://ismail_keyvan:<PASSWORD>@cluster0.cah1y.mongodb.net/answerSporComment?retryWrites=true&w=majority")
+    // <PASSWORD> kısmına şifre girilmeli
+        .then(() => {
         console.log("MongoDb connected")
     })
     .catch(err => {
